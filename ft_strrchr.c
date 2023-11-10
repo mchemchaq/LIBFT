1a4897022d9bf37371e6e0c4fff29c320b43f752
Maryem Chemchaq <mchemcha@e2r11p2.1337.ma>
2023-11-10 18:36:43 +0100

    fix ft_strrchr

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,25 +6,44 @@
 /*   By: mchemcha <mchemcha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/06 19:54:34 by mchemcha          #+#    #+#             */
-/*   Updated: 2023/11/10 18:17:17 by mchemcha         ###   ########.fr       */
+/*   Updated: 2023/11/10 18:35:40 by mchemcha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+// char *ft_strrchr(const char *s, int c)
+// {
+//     unsigned int  i;
+//     char *rep;
+//     rep = NULL;
+//     i = 0;
+//     if (c == 0)
+//         return ("");
+//     while(s[i])
+//     {     
+//         if (s[i] == (char ) c)
+//             rep = ((char*)&s[i]);
+//         i++;
+//     }
+//     return (rep);
+// }
+
 char *ft_strrchr(const char *s, int c)
 {
-    unsigned int  i;
+    int  i;
+    i = ft_strlen(s);
     char *rep;
+
     rep = NULL;
-    i = 0;
-    if (c == 0)
-        return ("");
-    while(s[i])
+    while(i >= 0)
     {     
         if (s[i] == (char ) c)
-            rep = ((char*)&s[i]);
-        i++;
+        {
+            rep = ((char*) s + i);
+            break;
+        }
+        i--;
     }
     return (rep);
 }
@@ -33,9 +52,9 @@ char *ft_strrchr(const char *s, int c)
 
 // int main() 
 // {
-//     const char *str = "";
+//     const char *str = "marihh kup;kng";
 //     //char *ptr = strrchr(str, 'o');
-//     char *p2 = ft_strrchr(str, '\0');
+//     char *p2 = ft_strrchr(str, 'k');
 //     //printf("%s", ptr);
 //     printf("%s", p2);
 //     return 0;
